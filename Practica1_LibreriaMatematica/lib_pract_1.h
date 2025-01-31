06069e4aec04fdffc2e25d05923334bb8facfb40
s-ol-jun <148479598+s-ol-jun@users.noreply.github.com>
2025-01-31 11:28:12 +0100

    Primeros archivos
    Creada carpeta + archivos render y libreria. Añadidas estructuras matriz y vector + cabeceras funciones vector

---
+++ Practica1_LibreriaMatematica/lib_pract_1.h
@@ -0,0 +1,37 @@
+#pragma once
+#define M_PI 3.14159265358979323846
+#include <iostream>
+
+namespace libreriaPractica1 {
+	typedef struct {
+		float x, y, z, w;
+	} Vector4f;
+
+	typedef struct {
+	    float matrix[4][4];     //Pone "estructura" en el documento, asumo que sería esto a lo que se refiere
+	} Matrix4x4f;
+
+    //Constructor de estructuras vector4f
+    inline Vector4f make_vector4f(float x, float y, float z,float w) {}
+
+    //Función para normalizar vectores
+    inline Vector4f normalize(Vector4f v) {}
+    
+    //Multiplicación escalar (Dot product)
+    inline vector4f operator* (vector4f v1, float esc) {}
+
+    //Multiplicación vectorial (Cross product)
+	inline float operator* (vector4f v1, vector4f v2) {
+		float res = v1.x * v2.x + 
+					v1.y * v2.y + 
+					v1.z * v2.z + 
+					v1.w * v2.w;
+		return res;
+	}
+
+    //Suma de vectores
+    inline vector4f operator+ (vector4f v1, vector4f v2) {}
+
+    //Resta de vectores
+    inline vector4f operator- (vector4f v1, vector4f v2) {}
+}
